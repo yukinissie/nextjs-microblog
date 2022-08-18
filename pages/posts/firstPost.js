66ad6788b1efab3ae971a0d2162df939ea435313
yukinissie <bigtree2490@outlook.jp>
2022-08-18 15:36:02 +0900

    17

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+
 export default function firstPost() {
   return (
     <div>
+      <Head>
+        <title>first post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <h1>最初の投稿</h1>
-      <h2>ホームへ戻る</h2>
+      <Link href="/">ホームへ戻る</Link>
     </div>
   );
 }
