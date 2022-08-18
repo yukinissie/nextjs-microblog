9bfba7ed2989c56428b96e67f9202c29431d4921
yukinissie <bigtree2490@outlook.jp>
2022-08-18 16:33:27 +0900

    20

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 const name = "Shin Code";
 export const siteTitle = "Next.js blog";
@@ -11,8 +12,8 @@ function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" />
-        <h1>{name}</h1>
+        <img src="/images/profile.png" className={utilStyles.borderCircle} />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
